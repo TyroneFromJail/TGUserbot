64e98fd83f757168a22ebe119afa62d9a2e83bc6
TyroneFromJail <42723331+TyroneFromJail@users.noreply.github.com>
2024-08-07 20:25:14 +0200

    Update musicdl.py

--- musicdl.py
+++ musicdl.py
@@ -19,7 +19,7 @@ from telethon.tl.types import Document
 from .. import loader, utils
 
 
-class MusicDLLibs(loader.Library):
+class MusicDLLib(loader.Library):
     developer = "@hikariatama"
     version = (1, 0, 19)
 
